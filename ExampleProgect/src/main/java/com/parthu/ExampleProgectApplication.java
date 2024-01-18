b87c7cec2a4007277fa3f1e00517aca809902aac
Parthasaradhi <parthasaradhi.git1506@gmail.com>
2024-01-18 22:46:35 +0530

    java

---
+++ ExampleProgect/src/main/java/com/parthu/ExampleProgectApplication.java
@@ -0,0 +1,13 @@
+package com.parthu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProgectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleProgectApplication.class, args);
+	}
+
+}
